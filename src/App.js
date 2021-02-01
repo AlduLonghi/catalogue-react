a2f6303f154665b2bf45db47f09a1796ef011d08
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-01 12:26:57 -0300

    Setup stylelint, add redux, react-redux and axios dependencies

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <p>Hello!</p>
-      </header>
+      hello
     </div>
   );
 }
