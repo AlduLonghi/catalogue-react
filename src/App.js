db1f1eae40bb208f8a67e9dd196f7db31c53ac58
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-02 04:40:47 -0300

    Create components, containers, assets and redux folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import axios from 'axios';
+
+async function getData() {
+  const request = await axios.get('https://openaccess-api.clevelandart.org/api/artworks?limit=5', {})
+    .then(res => res.data);
+  return request.data;
+}
 
 function App() {
+  console.log(getData());
   return (
     <div className="App">
-      hello
+      hhhh
     </div>
   );
 }
