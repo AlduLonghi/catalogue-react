3183c05526432ef10f8fe788edc647b0095cc61c
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-02 07:15:02 -0300

    Display components

--- src/containers/ArtworksContainer.js
+++ src/containers/ArtworksContainer.js
@@ -2,25 +2,58 @@ import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import fetchArtworks from '../redux/actions/artworks';
+import '../styles/App.css';
 
-const ArtworksContainer = ({ fetchArtworks }) => {
+const ArtworksContainer = ({ fetchArtworks, artworks }) => {
   useEffect(() => {
     fetchArtworks();
   }, []);
 
-  return (
+  return artworks.length ? (
+    <div className="row">
+      <div className="col-xl-4 col-md-6 col-12">
+        <img src={artworks[0].images.web.url} alt="jaja" />
+        <img src={artworks[1].images.web.url} alt="jaja" />
+        <img src={artworks[2].images.web.url} alt="jaja" />
+        <img src={artworks[3].images.web.url} alt="jaja" />
+        <img src={artworks[4].images.web.url} alt="jaja" />
+      </div>
+      <div className="col-xl-4 col-md-6 col-12">
+        <img src={artworks[5].images.web.url} alt="jaja" />
+        <img src={artworks[6].images.web.url} alt="jaja" />
+        <img src={artworks[7].images.web.url} alt="jaja" />
+        <img src={artworks[8].images.web.url} alt="jaja" />
+      </div>
+      <div className="col-xl-4 col-md-6 col-12">
+        <img src={artworks[9].images.web.url} alt="jaja" />
+        <img src={artworks[10].images.web.url} alt="jaja" />
+        <img src={artworks[11].images.web.url} alt="jaja" />
+        <img src={artworks[12].images.web.url} alt="jaja" />
+      </div>
+    </div>
+  ) : (
     <div>
-      hello
+      loading
     </div>
   );
 };
 
 ArtworksContainer.propTypes = {
   fetchArtworks: PropTypes.func,
+  artworks: PropTypes.instanceOf(Array),
 };
 
 ArtworksContainer.defaultProps = {
   fetchArtworks: null,
+  artworks: [],
 };
 
-export default connect(null, { fetchArtworks })(ArtworksContainer);
+const mapStateToProps = state => ({
+  artworks: state.artworks.artworks,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchArtworks: () => dispatch(fetchArtworks()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArtworksContainer);
--- src/redux/actions/artworks.js
+++ src/redux/actions/artworks.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import FETCH_ARTWORKS from './types';
 
 const fetchArtworks = () => dispatch => {
-  axios.get('https://openaccess-api.clevelandart.org/api/artworks/?limit=5&has_image=1')
+  axios.get('https://openaccess-api.clevelandart.org/api/artworks/?limit=25&has_image=1')
     .then(res => dispatch({
       type: FETCH_ARTWORKS,
-      artworks: res,
+      artworks: res.data.data,
     }));
 };
 
