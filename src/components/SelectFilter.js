6d077ffc66ce9fb3af3a6aa556402ea4e4f99080
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-03 07:52:56 -0300

    Add handleChange method for select filter

--- src/components/SelectFilter.js
+++ src/components/SelectFilter.js
@@ -1,13 +1,27 @@
-const SelectFilter = () => (
-  <select className="form-select">
-    <option value="All">All</option>
-    <option value="Painting">Painting</option>
-    <option value="Metalwork">Metalwork</option>
-    <option value="Sculpture">Sculpture</option>
-    <option value="Ceramic">Ceramic</option>
-    <option value="Bound volume">Bound volume</option>
-    <option value="Miscellaneous">Miscellaneous</option>
-  </select>
-);
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import changeFilter from '../redux/actions/selectFilter';
 
-export default SelectFilter;
+const SelectFilter = ({ changeFilter }) => {
+  const values = ['All', 'Painting', 'Metalwork', 'Sculpture', 'Ceramic', 'Bound volume', 'Miscellaneous'];
+
+  const handleChange = e => {
+    changeFilter(e.target.value);
+  };
+
+  return (
+    <select className="form-select" onChange={handleChange}>
+      {values.map(val => <option key={val} value={val}>{val}</option>)}
+    </select>
+  );
+};
+
+SelectFilter.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = {
+  changeFilter,
+};
+
+export default connect(null, mapDispatchToProps)(SelectFilter);
